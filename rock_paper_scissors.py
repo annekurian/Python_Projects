90d6d9c6487b35711de9805e5be646bec29fa108
annekurian <anne.kurian14@gmail.com>
2026-03-26 10:45:10 +0530

    Refactoring: Modularizing code

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -11,30 +11,41 @@ import random
 choices=('r','p','s') # using a tuple so that it is read only and it is not accidentally modified
 emojis = {'r':'🪨', 'p':'📄', 's':'✂️'}
 
-while True:
-    user_choice = input("Rock, paper, or scissors? (r/p/s): ").lower()
-    if user_choice not in choices:
-        print("Invalid choice!")
-        continue
-    computer_choice = random.choice(choices)
+def get_user_choice():
+    while True:
+        user_choice = input("Rock, paper, or scissors? (r/p/s): ").lower()
+        if user_choice in choices:
+            return user_choice
+        else:
+            print("Invalid choice!")
 
+def display_choices(user_choice, computer_choice):
     print(f"You chose {emojis[user_choice]}")
     print(f"Computer chose {emojis[computer_choice]}")
-    
+
+def determine_winner(user_choice, computer_choice):
     if user_choice == computer_choice:
-        print("It is a tie")
+        print("It is a draw")
     elif (user_choice, computer_choice) in [('r','s'), ('p','r'), ('s','p')]:
         print("You won")
     else:
         print("You lose")
 
-    is_continue = input("Continue? (y/n): ").lower()
-    if not is_continue == "n" and not is_continue == "y":
-        print("Invalid choice!")
-        break
-    elif is_continue == "n":
-        break
-    
+def play_game():
+    while True:
+        user_choice = get_user_choice()
 
-    
+        computer_choice = random.choice(choices)
 
+        display_choices(user_choice, computer_choice)
+        
+        determine_winner(user_choice, computer_choice)
+
+        is_continue = input("Continue? (y/n): ").lower()
+        if not is_continue == "n" and not is_continue == "y":
+            print("Invalid choice!")
+            break
+        elif is_continue == "n":
+            break
+    
+play_game()
