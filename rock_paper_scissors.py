4afaa30c53fd7175fb718980c1b3aa84f7a173dc
annekurian <anne.kurian14@gmail.com>
2026-03-26 10:42:18 +0530

    Order of if statements changed for better readability

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -10,20 +10,23 @@ import random
 
 choices=('r','p','s') # using a tuple so that it is read only and it is not accidentally modified
 emojis = {'r':'🪨', 'p':'📄', 's':'✂️'}
+
 while True:
     user_choice = input("Rock, paper, or scissors? (r/p/s): ").lower()
-    if user_choice in choices:
-        computer_choice = random.choice(choices)
-        print(f"You chose {emojis[user_choice]}")
-        print(f"Computer chose {emojis[computer_choice]}")
-        if (user_choice, computer_choice) in [('r','s'), ('p','r'), ('s','p')]:
-            print("You won")
-        elif (user_choice, computer_choice) in [('r','p'), ('p','s'), ('s','r')]:
-            print("You lose")
-        elif user_choice == computer_choice:
-            print("It is a draw")
-    else:
+    if user_choice not in choices:
         print("Invalid choice!")
+        continue
+    computer_choice = random.choice(choices)
+
+    print(f"You chose {emojis[user_choice]}")
+    print(f"Computer chose {emojis[computer_choice]}")
+    
+    if user_choice == computer_choice:
+        print("It is a tie")
+    elif (user_choice, computer_choice) in [('r','s'), ('p','r'), ('s','p')]:
+        print("You won")
+    else:
+        print("You lose")
 
     is_continue = input("Continue? (y/n): ").lower()
     if not is_continue == "n" and not is_continue == "y":
